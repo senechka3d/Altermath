0cc7289c0637955d59ee64e33e2b585cf58e2441
senechka3d <senechka3d@gmail.com>
2026-09-25 15:24:14 +0000

    feat: add mathematical constants

--- src/altermath/__init__.py
+++ src/altermath/__init__.py
@@ -1,5 +1,6 @@
 from .general import (add, subtract, multiply, divide, mod, fdiv, square,
                        cube, power, sqrt, root)
+from .constants import PI, E, PHI
 
 
 __version__ = "1.0"
